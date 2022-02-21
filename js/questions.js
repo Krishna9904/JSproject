bb6a8e1ba5312ae192697206ee4e0cc625aca9ec
unknown <krishnarao.amperayani@gmail.com>
2022-02-21 19:41:42 +0530

    first

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What number best completes the analogy -  8:4 as 10:?",
+    answer: "5",
+    options: [
+      "8",
+      "5",
+      "4",
+      "none"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Your mother’s brother’s only brother-in-law is asleep on your couch. Who is asleep on your couch?",
+    answer: "Your Father",
+    options: [
+      "Your Mother",
+      "Your Father",
+      "Your Brother-in-law",
+      "Your Brother"
+    ]
+  },
+    {
+    numb: 3,
+    question: "I have three apples. If you take away two from me, how many do you have?",
+    answer: "2",
+    options: [
+      "2",
+      "1",
+      "3",
+      "0"
+    ]
+  },{
+    numb: 4,
+    question: "'Senile Felines' is a palindrome?",
+    answer: "True",
+    options: [
+      "True",
+      "False",
+      "Can't predict",
+      "Don't know"
+    ]
+  },
+    
+    {
+    numb: 5,
+    question: "3, 7, 13, 21, 31. What number comes next in the sequence?",
+    answer: "43",
+    options: [
+      "74",
+      "37",
+      "43",
+      "11"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
